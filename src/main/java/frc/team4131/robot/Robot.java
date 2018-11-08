c4f8bc3b81a82812c177c0480079c90fb4b6cbe0
sanjami15 <sanjana.addanki@gmail.com>
2018-11-08 15:54:05 -0800

    drive base code

--- src/main/java/frc/team4131/robot/Robot.java
+++ src/main/java/frc/team4131/robot/Robot.java
@@ -1,33 +1,41 @@
 package frc.team4131.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.command.Scheduler;
 
 public class Robot extends IterativeRobot {
+    TalonSRX motor1 = new TalonSRX(1);
+    TalonSRX motor2 = new TalonSRX(2);
+    TalonSRX motor3 = new TalonSRX(3);
+    TalonSRX motor4 = new TalonSRX(4);
+    XboxController controller = new XboxController(1);
+
+
     @Override
     public void robotInit() { }
 
     @Override
-    public void disabledInit() { }
-
-    @Override
-    public void autonomousInit() { }
-
-    @Override
     public void teleopInit() { }
 
-    @Override
-    public void testInit() { }
-
-
-    @Override
-    public void disabledPeriodic() { }
     
     @Override
     public void autonomousPeriodic() { }
 
     @Override
-    public void teleopPeriodic() { }
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+        drive();
+    }
 
-    @Override
-    public void testPeriodic() { }
+public void drive() {
+    double straight = controller.getRawAxis(0);
+    double rotate = controller.getRawAxis(5);
+    motor1.set(ControlMode.PercentOutput, rotate + straight);
+    motor2.set(ControlMode.PercentOutput, rotate + straight);
+    motor3.set(ControlMode.PercentOutput, rotate - straight);
+    motor4.set(ControlMode.PercentOutput, rotate - straight);
+}
 }
